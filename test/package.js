353f3b33a9e351133418e8685139530d3f36f19a
Jim Manton <jrman@risebroadband.net>
2022-12-11 09:59:34 -0700

    Take out stringify json just for the colors. Unnecessary.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "10.1.0",
+  "version": "10.1.1",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^4.3.3",
