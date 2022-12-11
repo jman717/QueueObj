6636f6fbe4cb71a66740b964fa080941e4c197b0
Jim Manton <jrman@risebroadband.net>
2022-12-11 11:11:43 -0700

    Remove json object from execution time.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "10.1.1",
+  "version": "10.1.2",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^4.3.3",
