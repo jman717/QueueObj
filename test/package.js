24fb28012bc7b7e2d09cb58f19bc1c76e3464769
Jim Manton <jrman@risebroadband.net>
2023-02-01 19:39:09 -0700

    add github actions

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "12.0.5",
+  "version": "12.0.6",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^4.3.7",
