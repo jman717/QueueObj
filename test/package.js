fae378a14bd4fb873ca5124a4f538007027a1302
Jim Manton <jrman@risebroadband.net>
2023-02-01 21:46:11 -0700

    update yml

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "12.0.16",
+  "version": "12.0.17",
   "bundleDependencies": false,
   "dependencies": {
     "chai": "^4.3.7",
